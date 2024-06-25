629e95a5ffba2e611651b663771ff01736e44dc0
Chiamaka Prosper Nkwazema <149527450+prosperpaul@users.noreply.github.com>
2024-06-25 09:51:07 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Who is a woman?</h1>
+      <p>
+        <b>I am a good girl.</b>
+      </p>
+      <ol>
+        <h4>Names of Great Women</h4>
+        <li>Amaka</li>
+        <li>Prosper</li>
+        <li>Anita</li>
+        <li>Mariam</li>
+        <li>KossyGift</li>
+      </ol>
     </div>
   );
 }
